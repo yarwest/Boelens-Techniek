659a45d5fa0374cd684f19b880b8c9d8148e4b6a
Yarno Boelens <yarno@print.com>
2018-07-17 01:48:28 +0200

    Messing around with a slightly more structured class.

--- js/validation.js
+++ js/validation.js
@@ -1,64 +1,85 @@
-const emailInput = document.getElementById("contactFormEmailInput"),
-    phoneInput = document.getElementById("contactFormPhoneInput"),
-    nameInput = document.getElementById("contactFormNameInput"),
-    messageInput = document.getElementById("contactFormMessageInput"),
-    validationHelper = document.getElementById("contactFormValidationHelper"),
+class FormObject {
+    constructor(input) {
+        this.input = input;
+    }
+    get value() {
+        return this.input.value.trim();
+    }
+    setValid() {
+        this.input.classList.remove("is-invalid");
+        this.input.classList.add("is-valid");
+    }
+    setInvalid() {
+        this.input.classList.add("is-invalid");
+        this.input.classList.remove("is-valid");
+    }
+    setNeutral() {
+        this.input.classList.remove("is-invalid");
+        this.input.classList.remove("is-valid");
+    }
+}
+
+const validationHelper = document.getElementById("contactFormValidationHelper"),
     form = document.getElementById("contactForm");
+
+const emailObject = new FormObject(document.getElementById("contactFormNameInput")),
+    phoneObject = new FormObject(document.getElementById("contactFormPhoneInput")),
+    nameObject = new FormObject(document.getElementById("contactFormNameInput")),
+    messageObject = new FormObject(document.getElementById("contactFormMessageInput"));
 
 function validateContactNewInput() {
     let valid = true;
-    const nameValue = nameInput.value.trim(),
-        messageValue = messageInput.value.trim(),
-        emailValue = emailInput.value.trim(),
-        phoneValue = phoneInput.value.trim();
-    if(nameValue.length === 0) {
+
+    if(nameObject.value.length === 0) {
         console.log("no name");
-        nameInput.classList.add("is-invalid");
+        nameObject.setInvalid();
         valid = false;
     } else {
-        if(!/^([a-z-,.']+)$/i.test(nameValue)) {
+        if(!/^([a-z-,.']+)$/i.test(nameObject.value)) {
             console.log("name invalid");
-            nameInput.classList.add("is-invalid");
+            nameObject.setInvalid();
             valid = false;
         } else {
-            nameInput.classList.remove("is-invalid");
-            nameInput.classList.add("is-valid");
+            nameObject.setValid();
         }
     }
-    if(messageValue.length === 0) {
+    if(messageObject.value.length === 0) {
         console.log("no message");
-        messageInput.classList.add("is-invalid");
+        messageObject.setInvalid();
         valid = false;
     } else {
-        messageInput.classList.remove("is-invalid");
-        messageInput.classList.add("is-valid");
+        messageObject.setValid();
     }
-    if(emailValue.length === 0 && phoneValue.length === 0) {
+    if(emailObject.value.length === 0 && phoneObject.value.length === 0) {
         console.log("email or phone not present");
-        emailInput.classList.add("is-invalid");
-        phoneInput.classList.add("is-invalid");
-        validationHelper.classList.add("is-invalid");
+        emailObject.setInvalid();
+        phoneObject.setInvalid();
+        setInvalid(validationHelper);
         valid = false;
     } else {
-        validationHelper.classList.remove("is-invalid");
-        if(emailValue.length > 0) {
-            if(!/^([a-z0-9]+@[a-z0-9]+([.][a-z0-9]+)?)$/i.test(emailValue)) {
+        setValid(validationHelper);
+        if(emailObject.value.length > 0) {
+            if(!/^([a-z0-9]+@[a-z0-9]+([.][a-z0-9]+)?)$/i.test(emailObject.value)) {
                 console.log("email invalid");
-                emailInput.classList.add("is-invalid");
+                emailObject.setInvalid();
                 valid = false;
             } else {
-                emailInput.classList.remove("is-invalid");
-                emailInput.classList.add("is-valid");
+                emailObject.setValid();
+                if(phoneObject.value.length === 0) {
+                    phoneObject.setNeutral();
+                }
             }
         }
-        if(phoneValue.length > 0) {
-            if(!/^([0-9 +]+)$/i.test(phoneValue)) {
+        if(phoneObject.value.length > 0) {
+            if(!/^([0-9 +]+)$/i.test(phoneObject.value)) {
                 console.log("phone invalid");
-                phoneInput.classList.add("is-invalid");
+                phoneObject.setInvalid();
                 valid = false;
             } else {
-                phoneInput.classList.remove("is-invalid");
-                phoneInput.classList.add("is-valid");
+                phoneObject.setValid();
+                if(emailObject.value.length === 0) {
+                    emailObject.setNeutral();
+                }
             }
         }
     }
@@ -70,23 +91,31 @@ function validateContactNewInput() {
 }
 
 function setAllInputValid() {
-    nameInput.classList.remove("is-invalid");
-    nameInput.classList.add("is-valid");
-    messageInput.classList.remove("is-invalid");
-    messageInput.classList.add("is-valid");
-    emailInput.classList.remove("is-invalid");
-    emailInput.classList.add("is-valid");
-    phoneInput.classList.remove("is-invalid");
-    phoneInput.classList.add("is-valid");
+    nameObject.setValid();
+    messageObject.setValid();
+    emailObject.setValid();
+    phoneObject.setValid();
     validationHelper.classList.remove("is-invalid");
 }
 
-emailInput.addEventListener('input', function() {
+function setValid() {
+    
+}
+
+function setInvalid() {
+
+}
+
+function removeValidation() {
+
+}
+
+//emailInput.addEventListener('input', function() {
     //validateContactNewInput();
-});
-phoneInput.addEventListener('input', function() {
+//});
+//phoneInput.addEventListener('input', function() {
     //validateContactNewInput();
-});
+//});
 
 window.addEventListener('load', function() {
     // Prevent submission
@@ -96,7 +125,8 @@ window.addEventListener('load', function() {
         if (!form.checkValidity() || !validationSuccessful) {
             event.preventDefault();
             event.stopPropagation();
+        } else {
+            form.classList.add('was-validated');
         }
-        form.classList.add('was-validated');
     }, false);
 }, false);
