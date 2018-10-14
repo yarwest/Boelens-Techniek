82a074aee2d4b050e22f13059a6088e5d0d05ff9
Yarno Boelens <yarno@print.com>
2018-10-14 23:16:40 +0200

    Mostly logic and styling for the collapsible list

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 Array.from(document.getElementsByClassName('collapsibleList')).forEach(element => {
-  console.log(element);
-  element.addEventListener('click', function(e) {
-    Array.from(document.getElementsByClassName('collapsibleListItems')).forEach(itemList => {
-      itemList.classList.add('hidden');
-    });
-    Array.from(element.children).forEach(child => {
-      if (child.classList.contains('collapsibleListItems')) {
-        child.classList.remove('hidden');
-      }
-    });
+  Array.from(element.children).forEach(child => {
+    if (child.classList.contains('collapsibleListHeader')) {
+      child.addEventListener('click', function(e) {
+        if (element.classList.contains('selected')) {
+          element.classList.remove('selected');
+        } else {
+          Array.from(document.getElementsByClassName('collapsibleList')).forEach(itemList => {
+            itemList.classList.remove('selected');
+          });
+          element.classList.add('selected');
+        }
+      });
+    }
   });
 });
